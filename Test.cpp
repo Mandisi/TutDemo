8c633f9894474ae08b6c9e6389fa0424b0552914
mandisi <212529699@stu.ukzn.ac.za>
2016-03-01 14:58:55 +0200

    Time class

---
+++ Test.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include"Time.h"
+
+using namespace std;
+
+int main()
+{
+	Time teaTime(10, 15, 0);
+	Time midnight;
+
+	int hours, mins, secs;
+	teaTime.print();
+	cout << endl;
+	midnight.print();
+
+	cout << endl;
+
+
+	teaTime.setTime(11, 0, 0);
+	teaTime.print();
+	cout << endl;
+
+	teaTime.getTime(hours, mins, secs);
+	cout << "Hours: " << hours << "Minutes: " << mins << "Seconds: " << secs << endl;
+
+	cout << endl;
+
+	cout << "TeaTime is at " << teaTime << "tomorrow" << endl;  //test overloadedstream inertion operator.
+
+}
