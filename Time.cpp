aae7ed71c74d18183e75d56bfc15ff5e7c925e74
Mandisi <212529699@stu.ukzn.ac.za>
2016-03-01 15:15:29 +0200

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -62,7 +62,7 @@ ostream &operator<<(ostream &output, const Time &time_in)
 using namespace std;
 int main ()
 {
-  for(int i = 1; i <= 100; i++)
+  for(int i = 1; i <= 20; i++)
   {
     if(i % 3 == 0 && i % 5 == 0)
       cout << "FizzBuzz" << endl;
